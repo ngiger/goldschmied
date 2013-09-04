c0430749881e927bf799abb9496de584a6991fd2
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-09-04 21:31:28 +0200

    Import läuft besser. Schema noch nicht definitiv

--- admin/import.rb
+++ admin/import.rb
@@ -6,109 +6,52 @@ $: << File.join(File.dirname(File.dirname(__FILE__)), 'lib')
 require 'data_schema'
 require 'dm-types'
 
-$inMemory = true
+$inMemory = false
 unless $inMemory
   puts "using development.db"
-  DataMapper::setup(:default, File.join('sqlite3://', Dir.pwd, 'development.db'))
+#  DataMapper::setup(:default, File.join('sqlite3://', Dir.pwd, 'tmp/development.db'))
+  DataMapper::setup(:default, File.join('sqlite3:///tmp/development.db'))
 else
   DataMapper.setup(:default, 'sqlite::memory:')
 end
 # A Postgres connection:
 # DataMapper.setup(:default, 'postgres://user:password@hostname/database')
 
-DataMapper.finalize
 require  'dm-migrations'
+DataMapper.finalize
 # DataMapper.auto_upgrade
-DataMapper.auto_upgrade!
-#  AdrNumIndex
-#  Firma
-#  Firmenzusatz
-#  Titel
-#  Vorname
-#  Name
-#  Strasse
-#  Ortzusatz
-#  Staat
-#  PLZ
-#  Ort
-
-#  Postadr_Text
-#  Postadr_Strasse
-#  Postadr_PO_Box
-#  PostadOrtzusatz
-#  Postadr_Staat
-#  Postadr_PLZ
-#  Postadr_Ort
-
-#  Adr02_Text
-#  Adr02_Firma
-#  Adr02Firmzusatz
-#  Adr02_Titel
-#  Adr02_Vorname
-#  Adr02_Name
-#  Adr02_Strasse
-#  Adr02_PO_Box
-#  Adr02_Ortzusatz
-#  Adr02_Staat
-#  Adr02_PLZ
-#  Adr02_Ort
-
-#  Tel01_von
-#  Tel01_Nummer
-#  Tel02_von
-#  Tel02_Nummer
-#  Tel03_von
-#  Tel03_Nummer
-#  Tel04_von
-#  Tel04_Nummer
-#  Fax01_von
-#  Fax01_Nummer
-#  Handy01_von
-#  Handy01_Nummer
-#  Handy02_von
-#  Handy02_Nummer
-#  Email01_von
-#  Email01_Text
-#  Email02_von
-#  Email02_Text
-
-#  Internet01
-#  Internet02
-#  AdrNum
-#  Code
-#  Sortier
-#  Fax02_von
-#  Fax02_Nummer
-#  Bild1
-#  Bild2
-#  Email03_von
-#  Email03_Text
-#  
-
+DataMapper.auto_upgrade! # cleans tables
+  
 $defaultLanguage = 'de'
-def addContactType(id, value, language = $defaultLanguage)
-  contactType = ContactType.new
-  contactType.language = language
-  contactType.id = id
-  contactType.value = value
+def addContactType(type, value, language = $defaultLanguage)
+  begin
+  contactType = ContactType.create(:type => type, :value => value, :language => language)
+  rescue  => e
+    puts "rescueing for contactType #{type} #{value}"
+    puts "Error was #{e.inspect}"
+  end
 end
 addContactType(:internet,     'Internet')
 addContactType(:email,        'E-Mail')
 addContactType(:fax,          'Fax')
 addContactType(:mobile,       'Handy')
 addContactType(:phone,        'Telefon')
+puts "Created #{ContactType.all.size} contactTypes"
 
-def addAdressType(id, value, language=$defaultLanguage)
-  addressType = AddressType.new
-  addressType.language = language
-  addressType.id = id
-  addressType.value = value
+def addAdressType(type, value, language=$defaultLanguage)
+  puts "addAdressType #{type} #{value}"
+  addressType = AddressType.create(:type => type, :value => value, :language => language)
+  puts addressType.save
+  pp addressType
+  rescue  => e
+    puts "rescueing for addressType #{type} #{value}"
+    puts "Error was #{e.inspect}"
 end
 addAdressType(:main,      'Hauptadresse')
 addAdressType(:bill_to,   'Rechnungsadresse')
 addAdressType(:delivery,  'Lieferadresse')
 addAdressType(:other,     'Weitere Adresse')
-
+puts "Created #{AddressType.all.size} addressTypes"
 
 address_datei = '/opt/RolfMueller/export/Address01.csv'
 count = 0
@@ -116,25 +59,120 @@ nrSkips= 0
 f = File.new(address_datei)
 f.set_encoding(Encoding::ISO_8859_15, Encoding::UTF_8)
 
+def addContactType(row, client, type, column)
+  unless row[column].eql?('') 
+    ContactInfo.create(:client_id => client.id, :type =>type, :value_1 => :row[column], :value_2 => :row[column+1])
+  end
+end
+
 f.readlines.each {
   |line|
     begin
       row = line.force_encoding('utf-8').split(',')
+      pp row.join('; ')
       count += 1
       unless /\d\d\d\d\.\d\d/.match(row[0])
         puts "\nSkipping row #{count}: #{row.join(',')}"
         nrSkips += 1
         next
       end
-      address = Address.new  
-      address.client_id     = address.adrNumIndex
-      address.vorname       = row[4] 
-      address.name          = row[5]
-      pp row[0..5]
-      pp address
-      unless address.save
+      client = Client.new  
+      client.adrNumIndex   = row[0].strip
+      unless row[1].eql?('') # Firma
+        client.isPerson      = false
+        client.vorname       = row[1] 
+        client.name          = row[2]
+        if row[4] != '' or row[5] != ''
+          puts "Bin auf Firma mit Name/Vorname nicht vorbereitet #{row}"
+#          break
+        end
+      else
+        client.isPerson      = true
+        client.vorname       = row[4] 
+        client.name          = row[5]
+      end      
+      client.bild1 = row[55]
+      client.bild2 = row[56]
+      client.save
+      if row[6] # 
+        address = Address.new
+        address.client_id     = client.id
+        address.strasse = row[6]
+        address.ortzusatz = row[7]
+        address.staat = row[8]
+        address.plz = row[9]
+        address.ort = row[10]
+        address.type = :main
+        unless address.save
+          puts "Konnte Adresse #{address.inspect} nicht speichern!"
+          exit 2
+        end
+      end
+
+      unless row[11].eql?('') # 
+        address = Address.new
+        address.client_id     = client.id
+        address.text = row[11]
+        address.strasse = row[12]
+        address.po_box = row[13]
+        address.staat = row[15]
+        address.plz = row[16]
+        address.ort = row[17]
+        address.type = :delivery
+        unless address.save
+          puts "Konnte Adresse #{address.inspect} nicht speichern!"
+          exit 2
+        end
+        # pp address; puts "Address neu gespeichert";  break
+      end
+
+      unless row[18].eql?('') # 
+        address = Address.new
+        address.client_id     = client.id
+        address.text = row[18]
+# address.adr02_firma = row[19]
+# address.adr02firmzusatz = row[20]
+# address.adr02_titel = row[21]
+# address.adr02_vorname = row[22]
+# address.adr02_name = row[23]
+        address.strasse = row[24]
+        address.po_box = row[25]
+        address.staat = row[27]
+        address.plz = row[28]
+        address.ort = row[29]
+        address.type = :bill_to
+        unless address.save
+          puts "Konnte Adresse #{address.inspect} nicht speichern!"
+          exit 2
+        end
+        # pp address; puts "Address neu gespeichert";  break
+      end
+
+      addContactType(row, client, :phone, 30)
+      addContactType(row, client, :phone, 32)
+      addContactType(row, client, :phone, 34)
+      addContactType(row, client, :phone, 36)
+      addContactType(row, client, :fax, 38)
+      addContactType(row, client, :fax, 53)
+      addContactType(row, client, :mobile, 40)
+      addContactType(row, client, :mobile, 42)
+      addContactType(row, client, :email, 44)
+      addContactType(row, client, :email, 46)
+      addContactType(row, client, :email, 57)
+      unless row[48].eql?('') 
+        ContactInfo.create(:client_id => client.id, :type =>:internet, :value_1 => '', :value_2 => :row[48])
+      end
+      unless row[49].eql?('') 
+        ContactInfo.create(:client_id => client.id, :type =>:internet, :value_1 => '', :value_2 => :row[49])
+      end
+
+# address.adrnum = row[50]
+# address.code = row[51]
+# address.sortier = row[52]
+
+      unless client.save
         puts "Saving of row #{row} failed!"
-        address.errors.each do |error|
+        client.errors.each do |error|
             #print the name of the invalid attribute
             puts error.attribute_name
         end
@@ -142,14 +180,15 @@ f.readlines.each {
       end
       $stdout.write('.')
       $stdout.puts if count % 80 == 0
-      break unless $inMemory or count > 10
+      # break unless $inMemory or count > 10
     rescue  => e
-      puts "rescueing"
+      puts "rescueing for row #{row}"
       puts "Error was #{e.inspect}"
+      puts puts $@.join("\n")
       break
     end
 }
-puts "Skip #{nrSkips} of #{count} lines"
-pp Address.all.size
-pp Address.all.first
-puts 'done'
+puts "Skip #{nrSkips} of #{count} lines. Erfasste 
+* #{sprintf('%4d', Client.all.size)} Kunden
+* #{sprintf('%4d', Address.all.size)} Adressen und 
+* #{sprintf('%4d', ContactInfo.all.size)} Kontakt-Informationen"
--- admin/lib/data_schema.rb
+++ admin/lib/data_schema.rb
@@ -3,14 +3,20 @@
 # Adressen head /opt/RolfMueller/export/Address01.csv
 # AdrNumIndex,Firma,Firmenzusatz,Titel,Vorname,Name,Strasse,Ortzusatz,Staat,PLZ,Ort,Postadr_Text,Postadr_Strasse,Postadr_PO_Box,PostadOrtzusatz,Postadr_Staat,Postadr_PLZ,Postadr_Ort,Adr02_Text,Adr02_Firma,Adr02Firmzusatz,Adr02_Titel,Adr02_Vorname,Adr02_Name,Adr02_Strasse,Adr02_PO_Box,Adr02_Ortzusatz,Adr02_Staat,Adr02_PLZ,Adr02_Ort,Tel01_von,Tel01_Nummer,Tel02_von,Tel02_Nummer,Tel03_von,Tel03_Nummer,Tel04_von,Tel04_Nummer,Fax01_von,Fax01_Nummer,Handy01_von,Handy01_Nummer,Handy02_von,Handy02_Nummer,Email01_von,Email01_Text,Email02_von,Email02_Text,Internet01,Internet02,AdrNum,Code,Sortier,Fax02_von,Fax02_Nummer,Bild1,Bild2,Email03_von,Email03_Text
 require 'dm-core'
+require 'dm-types'
 require 'dm-timestamps'
 require 'dm-validations'
 
+module Goldschmied
+  AdressTypes  = [ :main, :bill_to, :delivery, :other ]
+  ContactTypes = [ :internet, :email, :fax, :mobile, :phone ]
+end
+
 class Client
   include DataMapper::Resource
-  has n, :addresses     , :required => false
-  has n, :contactInfos  , :required => false
-
+  has n, :addresses,    :through => Resource
+  has n, :contactInfos, :through => Resource
+  
   property :id,           Serial     # primary serial key # AdrNumIndex  
   property :adrNumIndex,  String     # Rolf Muellers old id
   property :created_at,   DateTime
@@ -19,6 +25,7 @@ class Client
   property :updated_on,   Date
   property :isPerson,     Boolean     # to distinguish between enterprises and humans
   property :titel,        String
+  alias    :firmenname    :titel
   property :vorname,      String
   property :name,         String
   property :bild1,        String
@@ -26,28 +33,35 @@ class Client
     
 end
 
-# main, bill_to, delivery, other
-class AddressType
-  include DataMapper::Resource
-  property :language,   String, :key => true, :length => 3 # ISO-Code
-  property :id,         String, :key => true
-  property :value,      String, :length => 32
-end
 
 class ContactType
   include DataMapper::Resource
-  property :language,   String, :key => true, :length => 3 # ISO-Code
-  property :id,         String, :key => true
-  property :value,      String, :length => 32
+  property :id,         Serial
+  property :type, Enum[ :internet, :email, :fax, :mobile, :phone], :default => :internet, :unique_index => :name
+  property :language,   String, :length =>  3, :unique_index => :name, :auto_validation => true, :default => 'de' # ISO-Code
+  property :value,      String, :length => 32, :default => 'E-Mail'
 end
 
-# E-Mail, Fax, Mobile, etc
 class ContactInfo
   include DataMapper::Resource
-  belongs_to              :client,      :key => true
-  has 1,                  :contactType, :key => true
-  property :value_1,      String
-  property :value_2,      String
+  property :id,         Serial
+  property :created_at,   DateTime
+  property :created_on,   Date
+  property :updated_at,   DateTime
+  property :updated_on,   Date
+  belongs_to              :client
+  property :type,       Enum[ :internet, :email, :fax, :mobile, :phone], :default => :internet
+  property :value_1,    String, :length =>  3
+  property :value_2,    String, :length => 32
+end
+
+# main, bill_to, delivery, other
+class AddressType
+  include DataMapper::Resource
+  property :id,         Serial
+  property :type,       Enum[ :main, :bill_to, :delivery, :other ], :default => :main, :unique_index => :named_u
+  property :language,   String, :length =>  3, :unique_index => :named_u   # ISO-Code
+  property :value,      String, :length => 32
 end
 
 class Address
@@ -61,9 +75,12 @@ class Address
   property :updated_at,   DateTime
   property :updated_on,   Date
   
-  property :titel,        String
-  property :vorname,      String
-  property :name,         String
+  property :type,         Enum[ Goldschmied::AdressTypes ], :default => Goldschmied::AdressTypes.first
+  property :text,         String
+#  property :titel,        String
+#  property :vorname,      String
+#  property :name,         String
+  property :po_box,       String
   property :strasse,      String
   property :ortzusatz,    String
   property :staat,        String
